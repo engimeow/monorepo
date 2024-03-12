0b917ad90660c9a0e8f78ca3a055bd4c0193821c
2D0 <lo7733ve@naver.com>
2024-03-12 15:31:19 +0900

    remove lagacy, change signup with rsc

--- apps/puppy-ti/app/page.tsx
+++ apps/puppy-ti/app/page.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { LogoutButton } from "./logout";
-import { useAuth } from "./providers/authProvider";
-
 export default function Page(): JSX.Element {
-  const { user } = useAuth<"required">();
-
-  if (!user) {
-    return <div>loading...</div>;
-  }
-
-  return (
-    <div>
-      <LogoutButton />
-    </div>
-  );
+  return <div>landing</div>;
 }
--- apps/puppy-ti/app/sign-up/page.tsx
+++ apps/puppy-ti/app/sign-up/page.tsx
@@ -1,5 +1,39 @@
-import { ClientComponent } from "./page.client";
+import { cookies } from "next/headers";
+import { createClient } from "@puppy-ti/lib/utils/supabase/server";
 
 export default function SignUpPage() {
-  return <ClientComponent />;
+  const signUp = async (e: FormData) => {
+    "use server";
+    const email = e.get("email") as string;
+    const password = e.get("password") as string;
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    if (!email || !password) {
+      alert("이메일과 비밀번호를 입력해주세요.");
+      return;
+    }
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (!error) {
+      console.log("이메일을 확인해주세요.");
+    } else {
+      console.log(error);
+      alert("에러가 발생했습니다.");
+    }
+  };
+
+  return (
+    <div>
+      <form action={signUp}>
+        <h1>Sign Up</h1>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">전송</button>
+      </form>
+    </div>
+  );
 }

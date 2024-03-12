c46037bd9c2b0c818b47470f9482764e4b0cbcab
pooh <82954504+hello-pooh@users.noreply.github.com>
2024-03-12 19:00:30 +0900

    remove styled components (#28)

--- apps/puppy-ti/app/layout.tsx
+++ apps/puppy-ti/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import StyledJsxRegistry from "./registry";
 import { AuthProvider } from "./providers/authProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <StyledJsxRegistry>{children}</StyledJsxRegistry>
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );

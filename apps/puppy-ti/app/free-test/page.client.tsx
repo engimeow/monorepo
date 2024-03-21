22f0718adf5aa6cfda20fc6881dc596301b32c88
hello-pooh <alan911011.dev@gmail.com>
2024-03-21 13:44:12 +0900

    free test page 수정

--- apps/puppy-ti/app/free-test/page.client.tsx
+++ apps/puppy-ti/app/free-test/page.client.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import { chunk } from "@puppy-ti/lib/utils/array/chunk";
+import { clsx } from "clsx";
 
 type QUESTION_ROW = Database["public"]["Tables"]["questions"]["Row"];
 
@@ -16,28 +17,68 @@ interface UIProps {
   dogName?: string;
 }
 
+interface Score {
+  questionId: number;
+  score: number;
+}
+
 type USER_MBTI_HISTORY_ROW =
   Database["public"]["Tables"]["user_mbti_history"]["Row"];
 
 type MBTI_ROW = Database["public"]["Enums"]["mbti"];
 
 export const UI = ({ questions, authUser, dogName }: UIProps) => {
+  const router = useRouter();
+
   const [userMBTIHistory, setUserMBTIHistory] =
     useState<USER_MBTI_HISTORY_ROW>();
   const [steps, setSteps] = useState<number>(0);
   const chunkSize = useMemo(() => 5, []);
   const maxStep = useMemo(() => chunk(questions, chunkSize).length, []);
-  const [mbti, setMbti] = useState<MBTI_ROW>("ENFJ");
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [scores, setScores] = useState<Score[]>([]);
 
-  const router = useRouter();
+  const calculateMBTIScore = (): MBTI_ROW => {
+    const scoreMap = {
+      E: 0,
+      I: 0,
+      N: 0,
+      S: 0,
+      T: 0,
+      F: 0,
+      J: 0,
+      P: 0,
+    };
+
+    scores.forEach(({ questionId, score }) => {
+      const question = questions.find((question) => question.id === questionId);
+      if (question) {
+        scoreMap[question.mbti as keyof typeof scoreMap] += score;
+      }
+    });
+
+    const mbtiResult =
+      `${scoreMap["E"] > scoreMap["I"] ? "E" : "I"}` +
+      `${scoreMap["N"] > scoreMap["S"] ? "N" : "S"}` +
+      `${scoreMap["T"] > scoreMap["F"] ? "T" : "F"}` +
+      `${scoreMap["J"] > scoreMap["P"] ? "J" : "P"}`;
+
+    return mbtiResult as MBTI_ROW;
+  };
 
   const handleSubmit = async () => {
     try {
+      const allAnswered = isAllAnswered();
+      if (!allAnswered) {
+        alert("모든 질문에 답해주세요.");
+        return;
+      }
       if (!buttonDisabled) {
         setButtonDisabled(true);
 
-        const { data, error } = await invoke(dogName, mbti);
+        const finalMBTI = calculateMBTIScore();
+
+        const { data, error } = await invoke(dogName, finalMBTI);
 
         if (error) {
           throw error;
@@ -52,47 +93,132 @@ export const UI = ({ questions, authUser, dogName }: UIProps) => {
     }
   };
 
+  const isAllAnswered = () => {
+    const currentQuestions = chunk(questions, chunkSize)[steps];
+
+    if (!currentQuestions) {
+      return;
+    }
+
+    return currentQuestions.every((question) =>
+      scores.some((score) => score.questionId === question.id),
+    );
+  };
+
   const handleNext = () => {
-    setSteps(steps + 1);
+    const allAnswered = isAllAnswered();
+    if (allAnswered) {
+      setSteps(steps + 1);
+    } else {
+      alert("모든 질문에 답해주세요.");
+    }
   };
 
   const renderButton = useCallback(() => {
     if (steps === maxStep - 1) {
       return (
-        <button disabled={buttonDisabled} type="button" onClick={handleSubmit}>
-          submit
+        <button
+          className={clsx([
+            "block-shape button-shadow text-white block-txt bg-[#C4A5FA]",
+          ])}
+          disabled={buttonDisabled}
+          type="button"
+          onClick={handleSubmit}
+        >
+          결과보기
         </button>
       );
     }
 
     return (
-      <button type="button" onClick={handleNext}>
-        next
+      <button
+        className={clsx([
+          "block-shape button-shadow text-white block-txt bg-[#C4A5FA]",
+        ])}
+        disabled={buttonDisabled}
+        type="button"
+        onClick={handleSubmit}
+      >
+        다음
       </button>
     );
-  }, [steps]);
+  }, [steps, scores]);
 
   const renderQuestions = useCallback(() => {
-    // return (
-    //   <ul>
-    //     {questions.map((question) => (
-    //       <li key={question.id}>{question.question}</li>
-    //     ))}
-    //   </ul>
-    // );
     const questionsByStep = chunk(questions, chunkSize)[steps];
+
+    const isScoreChecked = (
+      selectedScore: number | undefined,
+      currentScore: number,
+    ) => {
+      if (selectedScore === undefined) return false;
+
+      if (selectedScore >= 0)
+        return currentScore >= 0 && currentScore <= selectedScore;
+
+      return currentScore <= 0 && currentScore >= selectedScore;
+    };
+
     if (questionsByStep) {
       return (
-        <ul>
+        <ul className="space-y-7">
           {questionsByStep.map((question) => (
-            <li key={question.id}>{question.question}</li>
+            <li className="bg-white pb-4 sm:pb-10" key={question.id}>
+              <div className="flex flex-row text-white text-center text-2xl">
+                <span className="bg-[#9E446F] w-full">아니다</span>
+                <span className="bg-[#7846D0] w-full">그렇다</span>
+              </div>
+              <p className="text-[#333333] text-3xl mt-4 sm:mt-6 mb-3 px-3 sm:px-12">
+                {question.question}
+              </p>
+              <div className="flex flex-row justify-between px-3 sm:px-12">
+                {[-3, -2, -1, 0, 1, 2, 3].map((score) => {
+                  const selectedScoreObj = scores.find(
+                    (s) => s.questionId === question.id,
+                  );
+                  const selectedScore = selectedScoreObj
+                    ? selectedScoreObj.score
+                    : undefined;
+
+                  const color =
+                    Math.sign(score) === 1
+                      ? "purple"
+                      : Math.sign(score) === -1
+                        ? "magenta"
+                        : "gray";
+
+                  return (
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => handleScoreChange(question.id, score)}
+                    >
+                      <Image
+                        key={`${question.id}${score}`}
+                        src={`/footprint/footprint_${color}_${isScoreChecked(selectedScore, score) ? "filled" : "outline"}.svg`}
+                        alt={`/footprint/footprint_${color}_${isScoreChecked(selectedScore, score) ? "filled" : "outline"}`}
+                        width={0}
+                        height={0}
+                        className="w-[32px] h-[32px] sm:w-[65px] sm:h-[65px]"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </li>
           ))}
         </ul>
       );
     }
 
     return;
-  }, [steps]);
+  }, [steps, scores]);
+
+  const handleScoreChange = (questionId: number, score: number) => {
+    const updatedScores = scores.filter((s) => s.questionId !== questionId);
+    updatedScores.push({ questionId, score });
+    setScores(updatedScores);
+    console.log("click");
+  };
 
   useEffect(() => {
     if (userMBTIHistory) {
@@ -122,8 +248,10 @@ export const UI = ({ questions, authUser, dogName }: UIProps) => {
         className="absolute z-[-1] top-0 right-0"
         priority
       />
-      {renderQuestions()}
-      {renderButton()}
+      <div className="flex-col-m w-full page-inner">
+        {renderQuestions()}
+        {renderButton()}
+      </div>
     </div>
   );
 };
